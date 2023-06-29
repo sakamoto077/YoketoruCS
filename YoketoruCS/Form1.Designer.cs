c022cd1139ef47075c6321f1d483a2d09db978b3
sakamoto <130636519+sakamoto077@users.noreply.github.com>
2023-06-29 16:12:05 +0900

    0629Yoketoru

--- YoketoruCS/Form1.Designer.cs
+++ YoketoruCS/Form1.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            labelTitle = new Label();
+            buttonStart = new Button();
+            buttonTitle = new Button();
+            labelGmeover = new Label();
+            labelclear = new Label();
+            tempPlayer = new Label();
+            labelHighScore = new Label();
+            labelCopyright = new Label();
+            tempObstacle = new Label();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 50;
+            timer1.Tick += timer1_Tick;
+            // 
+            // labelTitle
+            // 
+            labelTitle.AutoSize = true;
+            labelTitle.Font = new Font("Yu Gothic UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            labelTitle.Location = new Point(306, 90);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(189, 54);
+            labelTitle.TabIndex = 0;
+            labelTitle.Text = "よけとるCS";
+            labelTitle.Click += label1_Click;
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(349, 234);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(103, 42);
+            buttonStart.TabIndex = 1;
+            buttonStart.Text = "スタート!!";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += button1_Click;
+            // 
+            // buttonTitle
+            // 
+            buttonTitle.Location = new Point(349, 320);
+            buttonTitle.Name = "buttonTitle";
+            buttonTitle.Size = new Size(103, 39);
+            buttonTitle.TabIndex = 2;
+            buttonTitle.Text = "タイトルへ";
+            buttonTitle.UseVisualStyleBackColor = true;
+            buttonTitle.Click += button2_Click;
+            // 
+            // labelGmeover
+            // 
+            labelGmeover.AutoSize = true;
+            labelGmeover.Font = new Font("Yu Gothic UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            labelGmeover.Location = new Point(284, 162);
+            labelGmeover.Name = "labelGmeover";
+            labelGmeover.Size = new Size(241, 54);
+            labelGmeover.TabIndex = 3;
+            labelGmeover.Text = "GAME OVER";
+            // 
+            // labelclear
+            // 
+            labelclear.AutoSize = true;
+            labelclear.Font = new Font("Yu Gothic UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            labelclear.Location = new Point(317, 128);
+            labelclear.Name = "labelclear";
+            labelclear.Size = new Size(159, 54);
+            labelclear.TabIndex = 4;
+            labelclear.Text = "CLEAR!!";
+            // 
+            // tempPlayer
+            // 
+            tempPlayer.AutoSize = true;
+            tempPlayer.Font = new Font("Yu Gothic UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            tempPlayer.Location = new Point(93, 188);
+            tempPlayer.Name = "tempPlayer";
+            tempPlayer.Size = new Size(57, 28);
+            tempPlayer.TabIndex = 5;
+            tempPlayer.Text = "( .    .)";
+            // 
+            // labelHighScore
+            // 
+            labelHighScore.AutoSize = true;
+            labelHighScore.Font = new Font("Yu Gothic UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            labelHighScore.Location = new Point(349, 22);
+            labelHighScore.Name = "labelHighScore";
+            labelHighScore.Size = new Size(92, 37);
+            labelHighScore.TabIndex = 6;
+            labelHighScore.Text = "00000";
+            // 
+            // labelCopyright
+            // 
+            labelCopyright.AutoSize = true;
+            labelCopyright.Font = new Font("Yu Gothic UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            labelCopyright.Location = new Point(172, 316);
+            labelCopyright.Name = "labelCopyright";
+            labelCopyright.Size = new Size(30, 25);
+            labelCopyright.TabIndex = 7;
+            labelCopyright.Text = "☆";
+            // 
+            // tempObstacle
+            // 
+            tempObstacle.AutoSize = true;
+            tempObstacle.Font = new Font("Yu Gothic UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            tempObstacle.Location = new Point(629, 147);
+            tempObstacle.Name = "tempObstacle";
+            tempObstacle.Size = new Size(30, 25);
+            tempObstacle.TabIndex = 8;
+            tempObstacle.Text = "★";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tempObstacle);
+            Controls.Add(labelCopyright);
+            Controls.Add(labelHighScore);
+            Controls.Add(tempPlayer);
+            Controls.Add(labelclear);
+            Controls.Add(labelGmeover);
+            Controls.Add(buttonTitle);
+            Controls.Add(buttonStart);
+            Controls.Add(labelTitle);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Label labelTitle;
+        private Button buttonStart;
+        private Button buttonTitle;
+        private Label labelGmeover;
+        private Label labelclear;
+        private Label tempPlayer;
+        private Label labelHighScore;
+        private Label labelCopyright;
+        private Label tempObstacle;
     }
 }
--- YoketoruCS/Form1.cs
+++ YoketoruCS/Form1.cs
@@ -1,10 +1,125 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Runtime.InteropServices;
+
 namespace YoketoruCS
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+        //—ñ‹“Žqenum
+        enum State
+        {
+            None = -1,
+            Title,
+            Game,
+            Gameover,
+            Clear
+        }
+
+        State nextState = State.Title;
+        State currentState = State.None;
+
+        int score;
+        int timer;
+        int highScore = 100;
+        int StartTimer => 200;
+        const int CStartTimer = 200;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ChangeState();
+            UpdateState();
+        }
+
+        void ChangeState()
+        {
+            if (nextState == State.None) return;
+
+            currentState = nextState;
+            nextState = State.None;
+
+            switch (currentState)
+            {
+                case State.Title:
+                    labelTitle.Visible = true;
+                    buttonStart.Visible = true;
+                    labelclear.Visible = false;
+                    labelHighScore.Visible = false;
+                    buttonTitle.Visible = false;
+                    labelGmeover.Visible = false;
+                    tempPlayer.Visible = false;
+                    tempObstacle.Visible = false;
+                    labelCopyright.Visible = true;
+                    break;
+
+                case State.Game:
+                    labelTitle.Visible = false;
+                    buttonStart.Visible = false;
+                    labelHighScore.Visible = false;
+                    labelCopyright.Visible = false;
+                    break;
+
+                case State.Gameover:
+                    labelGmeover.Visible = true;
+                    buttonTitle.Visible = true;
+                    labelHighScore.Visible = true;
+                    break;
+
+                case State.Clear:
+                    labelclear.Visible = true;
+                    buttonTitle.Visible = true;
+                    labelHighScore.Visible = true;
+                    break;
+            }
+        }
+
+        void UpdateState()
+        {
+            switch (currentState)
+            {
+                case State.Game:
+                    UpdateGame();
+                    break;
+            }
+        }
+
+        void UpdateGame()
+        {
+            if (GetAsyncKeyState((int)Keys.O) < 0)
+            {
+                nextState = State.Gameover;
+            }
+            if (GetAsyncKeyState((int)Keys.C) < 0)
+            {
+                nextState = State.Clear;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nextState = State.Game;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            nextState = State.Title;
         }
     }
 }
